ddd584d0e78a610d36965df96577060b34f0223a
Zulkarneyn Catak <zulkarneync@gmail.com>
2026-04-28 21:37:18 +0200

    Things that need to be added in controller commented in as well as created file for acquiring yfinance data

--- controller/controller.py
+++ controller/controller.py
@@ -20,8 +20,15 @@ def run():
         else:
             print("Invalid choice. Please try again.")
 
+        ## TODO: Add option to show current and historical price of each ticker and graphing (yfinance, matplotlib).s
 
+        ## TODO: View the current portfolio, displaying each asset's name, sector, asset class, quantity,purchase price, transaction value and current value. 
+        ## via matplotlib of gewoon in terminal ff kijken    
 
+        ## TODO: . See calculations for the total portfolio value and the (relative) weights of each asset including the option to see the same per asset class and sector.
+        ## 
+        ## TODO: Be able to perform a simulation over the upcoming fifteen years for the portfolio, demonstrating the impact of risk and uncertainty. Assume 100.000 simulated paths.
+        ## Monte Carlo simulation, DNB dataset gebruiken voor rente? of gewoon
 def add_asset(portfolio: Portfolio):
     ticker = pick_ticker()
     if ticker is None:
