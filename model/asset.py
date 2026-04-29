a2758918a676f855190b0170213ec57e0ad86d1b
Zulkarneyn Catak <zulkarneync@gmail.com>
2026-04-29 12:00:32 +0200

    Added currency to asset dataclass

--- model/asset.py
+++ model/asset.py
@@ -8,11 +8,11 @@ class Asset:
     asset_class: str
     quantity: float
     purchase_price: float
-
+    currency: str
 
     @property
     def transaction_value(self) -> float:
         return self.quantity * self.purchase_price
-    
+
     def current_value(self, current_price: float) -> float:
         return self.quantity * current_price
